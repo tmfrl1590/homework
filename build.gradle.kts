42aaa1c05f1be9d08198a329ca4c82e9b966ba31
tmfrl <tmfrl1590@gmail.com>
2023-12-12 15:27:33 +0900

    modify

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,6 @@ dependencies {
     // JUnit5
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
-
 }
 
 tasks.withType<KotlinCompile> {
